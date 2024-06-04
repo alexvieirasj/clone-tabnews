62f207e982c4fd66ac7849661a965b8d88405473
Alex Vieira <alex.vieira@philips.com>
2024-06-04 02:47:06 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>"Ninguém sabe o que o futuro reserva. É por isso que as possibilidades são infinitas." Steins;Gate</h1>
+    return <h1>"Ninguém sabe o que o futuro reserva. É por isso que as possibilidades são infinitas!" Steins;Gate</h1>
 }
 
 export default Home;
